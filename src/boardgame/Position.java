76adc453c3f3607083eacb22154271a9aad8ff3b
AlissonK1 <Alisson.kauan77@gmail.com>
2022-08-07 13:03:34 -0300

    Implementing and printing possible moves

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -1,11 +1,20 @@
 package boardgame;
 
 public class Position {
-    private int column;
+
     private int row;
+    private int column;
 
     public Position(int row, int column) {
+        this.row = row;
         this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
         this.row = row;
     }
 
@@ -17,12 +26,9 @@ public class Position {
         this.column = column;
     }
 
-    public int getRow() {
-        return row;
-    }
-
-    public void setRow(int row) {
+    public void setValues(int row, int column) {
         this.row = row;
+        this.column = column;
     }
 
     @Override
